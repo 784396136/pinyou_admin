8b2ab2b1ef6a7c29e08722083329315c182e2221
CunYe <784396136@qq.com>
2018-10-31 15:17:29 +0800

    添加分类表的迁移文件

---
+++ database/migrations/2018_10_31_151125_create_category_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('category', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('cat_name')->comment('分类名称');
+            $table->bigInteger('parent_id')->comment('父级ID');
+            $table->string('path')->comment('所有的上级分类ID');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('category');
+    }
+}
--- resources/views/product/category.blade.php
+++ resources/views/product/category.blade.php
@@ -1,151 +1,219 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
+
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <link href="/assets/css/bootstrap.min.css" rel="stylesheet" />
-        <link rel="stylesheet" href="/css/style.css"/>       
-        <link href="/assets/css/codemirror.css" rel="stylesheet">
-        <link rel="stylesheet" href="/assets/css/ace.min.css" />
-        <link rel="stylesheet" href="/Widget/zTree/css/zTreeStyle/zTreeStyle.css" type="text/css">
-        <link rel="stylesheet" href="/assets/css/font-awesome.min.css" />
-        
-		<!--[if IE 7]>
-		  <link rel="stylesheet" href="/assets/css/font-awesome-ie7.min.css" />
-		<![endif]-->
-        <!--[if lte IE 8]>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="renderer" content="webkit|ie-comp|ie-stand">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no" />
+	<meta http-equiv="Cache-Control" content="no-siteapp" />
+	<link href="/assets/css/bootstrap.min.css" rel="stylesheet" />
+	<link rel="stylesheet" href="/css/style.css" />
+	<link href="/assets/css/codemirror.css" rel="stylesheet">
+	<link rel="stylesheet" href="/assets/css/ace.min.css" />
+	<link rel="stylesheet" href="/font/css/font-awesome.min.css" />
+	<!--[if lte IE 8]>
 		  <link rel="stylesheet" href="/assets/css/ace-ie.min.css" />
 		<![endif]-->
-			<script src="/assets/js/jquery.min.js"></script>
-		<!-- <![endif]-->
-		<!--[if IE]>
-       <script src="/http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-        <![endif]-->
-		<!--[if !IE]> -->
-		<script type="text/javascript">
-			window.jQuery || document.write("<script src='/assets/js/jquery-2.0.3.min.js'>"+"<"+"/script>");
-		</script>
-		<!-- <![endif]-->
-		<!--[if IE]>
-<script type="text/javascript">
- window.jQuery || document.write("<script src='assets/js/jquery-1.10.2.min.js'>"+"<"+"/script>");
-</script>
-<![endif]-->
-        <script src="/assets/js/ace-elements.min.js"></script>
-		<script src="/assets/js/ace.min.js"></script>
-        <script src="/assets/js/bootstrap.min.js"></script>
-		<script src="/assets/js/typeahead-bs2.min.js"></script>
-        <script type="text/javascript" src="/Widget/zTree/js/jquery.ztree.all-3.5.min.js"></script> 
-        <script src="/js/lrtk.js" type="text/javascript" ></script>
-<title>分类管理</title>
+	<script src="/js/jquery-1.9.1.min.js"></script>
+	<script src="/assets/js/typeahead-bs2.min.js"></script>
+	<script src="/js/lrtk.js" type="text/javascript"></script>
+	<script src="/assets/js/jquery.dataTables.min.js"></script>
+	<script src="/assets/js/jquery.dataTables.bootstrap.js"></script>
+	<script src="/assets/layer/layer.js" type="text/javascript"></script>
+	<title>分类管理</title>
 </head>
 
 <body>
-<div class=" clearfix">
- <div id="category">
-    <div id="scrollsidebar" class="left_Treeview">
-    <div class="show_btn" id="rightArrow"><span></span></div>
-    <div class="widget-box side_content" >
-    <div class="side_title"><a title="隐藏" class="close_btn"><span></span></a></div>
-     <div class="side_list">
-      <div class="widget-header header-color-green2">
-          <h4 class="lighter smaller">产品类型列表</h4>
-      </div>
-      <div class="widget-body">
-          <div class="widget-main padding-8">
-              <div  id="treeDemo" class="ztree"></div>
-          </div>
-  </div>
-  </div>
-  </div>  
-  </div>
-<!---->
- <iframe ID="testIframe" Name="testIframe" FRAMEBORDER=0 SCROLLING=AUTO  SRC="{{Route('ProductCategory_add')}}" class="page_right_style"></iframe>
- </div>
-</div>
+	<div class="page-content clearfix">
+		<div class="sort_style">
+			<div class="border clearfix">
+				<span class="l_f">
+					<a href="javascript:ovid()" id="sort_add" class="btn btn-warning"><i class="fa fa-plus"></i> 添加分类</a>
+					<a href="javascript:ovid()" class="btn btn-danger"><i class="fa fa-trash"></i> 批量删除</a>
+				</span>
+				<span class="r_f">共：<b>5</b>类</span>
+			</div>
+			<div class="sort_list">
+				<table class="table table-striped table-bordered table-hover" id="sample-table">
+					<thead>
+						<tr>
+							<th width="25px"><label><input type="checkbox" class="ace"><span class="lbl"></span></label></th>
+							<th width="50px">ID</th>
+							<th width="100px">分类名称</th>
+							<th width="50px">加入时间</th>
+							<th width="150px">操作</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td><label><input type="checkbox" class="ace"><span class="lbl"></span></label></td>
+							<td>1</td>
+							<td>幻灯片</td>
+							<td>2016-6-29 12:34</td>
+							<td class="td-manage">
+								<a title="编辑" onclick="member_edit('编辑','member-add.html','4','','510')" href="javascript:;" class="btn btn-xs btn-info"><i
+									 class="fa fa-edit bigger-120"></i></a>
+								<a title="删除" href="javascript:;" onclick="member_del(this,'1')" class="btn btn-xs btn-warning"><i class="fa fa-trash  bigger-120"></i></a>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</div>
+	<!--添加分类-->
+	<div class="sort_style_add margin" id="sort_style_add" style="display:none">
+		<div class="">
+			<ul>
+				<li><label class="label_name">上级分类</label>
+					<div class="col-sm-9">
+						&nbsp;&nbsp;&nbsp;
+						<select id="p_id">
+							<option value="0">根级分类</option>
+						</select>
+					</div>
+				</li>
+				<li><label class="label_name">分类名称</label>
+					<div class="col-sm-9"><input id="cat_name" name="分类名称" type="text" id="form-field-1" placeholder="" class="col-xs-10 col-sm-5"></div>
+				</li>
+			</ul>
+		</div>
+	</div>
 </body>
+
 </html>
-<script type="text/javascript"> 
-$(function() { 
-	$("#category").fix({
-		float : 'left',
-		//minStatue : true,
-		skin : 'green',	
-		durationTime :false
+<script type="text/javascript">
+	$('#sort_add').on('click', function () {
+		layer.open({
+			type: 1,
+			title: '添加分类',
+			maxmin: true,
+			shadeClose: true, //点击遮罩关闭层
+			area: ['750px', ''],
+			content: $('#sort_style_add'),
+			btn: ['提交', '取消'],
+			yes: function (index, layero) {
+				var num = 0;
+				var str = "";
+				$(".sort_style_add input[type$='text']").each(function (n) {
+					if ($(this).val() == "") {
+
+						layer.alert(str += "" + $(this).attr("name") + "不能为空！\r\n", {
+							title: '提示框',
+							icon: 0,
+						});
+						num++;
+						return false;
+					}
+				});
+				if (num > 0) { return false; }
+				else {
+					var pid = $("#p_id").val();
+					var cat_name = $('#cat_name').val();
+					console.log(pid,cat_name);
+					layer.alert('添加成功！', {
+						title: '提示框',
+						icon: 1,
+					});
+					layer.close(index);
+				}
+			}
+		});
+	})
+
+
+	function checkLength(which) {
+		var maxChars = 200; //
+		if (which.value.length > maxChars) {
+			layer.open({
+				icon: 2,
+				title: '提示框',
+				content: '您出入的字数超多限制!',
+			});
+			// 超过限制的字数了就将 文本框中的内容按规定的字数 截取
+			which.value = which.value.substring(0, maxChars);
+			return false;
+		} else {
+			var curr = maxChars - which.value.length; //250 减去 当前输入的
+			document.getElementById("sy").innerHTML = curr.toString();
+			return true;
+		}
+	};
+	/*广告图片-停用*/
+	function member_stop(obj, id) {
+		layer.confirm('确认要关闭吗？', { icon: 0, }, function (index) {
+			$(obj).parents("tr").find(".td-manage").prepend('<a style="text-decoration:none" class="btn btn-xs " onClick="member_start(this,id)" href="javascript:;" title="显示"><i class="fa fa-close bigger-120"></i></a>');
+			$(obj).parents("tr").find(".td-status").html('<span class="label label-defaunt radius">已关闭</span>');
+			$(obj).remove();
+			layer.msg('关闭!', { icon: 5, time: 1000 });
+		});
+	}
+	/*广告图片-启用*/
+	function member_start(obj, id) {
+		layer.confirm('确认要显示吗？', { icon: 0, }, function (index) {
+			$(obj).parents("tr").find(".td-manage").prepend('<a style="text-decoration:none" class="btn btn-xs btn-success" onClick="member_stop(this,id)" href="javascript:;" title="关闭"><i class="fa fa-check  bigger-120"></i></a>');
+			$(obj).parents("tr").find(".td-status").html('<span class="label label-success radius">显示</span>');
+			$(obj).remove();
+			layer.msg('显示!', { icon: 6, time: 1000 });
+		});
+	}
+	/*广告图片-删除*/
+	function member_del(obj, id) {
+		layer.confirm('确认要删除吗？', { icon: 0, }, function (index) {
+			$(obj).parents("tr").remove();
+			layer.msg('已删除!', { icon: 1, time: 1000 });
+		});
+	}
+	//面包屑返回值
+	var index = parent.layer.getFrameIndex(window.name);
+	parent.layer.iframeAuto(index);
+	$('.Order_form ,.ads_link').on('click', function () {
+		var cname = $(this).attr("title");
+		var cnames = parent.$('.Current_page').html();
+		var herf = parent.$("#iframe").attr("src");
+		parent.$('#parentIframe span').html(cname);
+		parent.$('#parentIframe').css("display", "inline-block");
+		parent.$('.Current_page').attr("name", herf).css({ "color": "#4c8fbd", "cursor": "pointer" });
+		//parent.$('.Current_page').html("<a href='javascript:void(0)' name="+herf+">" + cnames + "</a>");
+		parent.layer.close(index);
+
 	});
-});
+	function AdlistOrders(id) {
+		window.location.href = "Ads_list.html?=" + id;
+	};
 </script>
 <script type="text/javascript">
-//初始化宽度、高度  
- $(".widget-box").height($(window).height()); 
- $(".page_right_style").width($(window).width()-220);
-  //当文档窗口发生改变时 触发  
-    $(window).resize(function(){
-	$(".widget-box").height($(window).height());
-	 $(".page_right_style").width($(window).width()-220);
-	})
- 
-/**************/
-var setting = {
-	view: {
-		dblClickExpand: false,
-		showLine: false,
-		selectedMulti: false
-	},
-	data: {
-		simpleData: {
-			enable:true,
-			idKey: "id",
-			pIdKey: "pId",
-			rootPId: ""
-		}
-	},
-	callback: {
-		beforeClick: function(treeId, treeNode) {
-			var zTree = $.fn.zTree.getZTreeObj("tree");
-			if (treeNode.isParent) {
-				zTree.expandNode(treeNode);
-				return false;
-			} else {
-				demoIframe.attr("src",treeNode.file + ".html");
-				return true;
-			}
-		}
-	}
-};
+	jQuery(function ($) {
+		var oTable1 = $('#sample-table').dataTable({
+			"aaSorting": [[1, "desc"]],//默认第几个排序
+			"bStateSave": true,//状态保存
+			"aoColumnDefs": [
+				//{"bVisible": false, "aTargets": [ 3 ]} //控制列的隐藏显示
+				{ "orderable": false, "aTargets": [0, 2, 4, 6, 7,] }// 制定列不参与排序
+			]
+		});
+		$('table th input:checkbox').on('click', function () {
+			var that = this;
+			$(this).closest('table').find('tr > td:first-child input:checkbox')
+				.each(function () {
+					this.checked = that.checked;
+					$(this).closest('tr').toggleClass('selected');
+				});
 
-var zNodes =[
-	{ id:1, pId:0, name:"商城分类列表", open:true},
-	{ id:11, pId:1, name:"蔬菜水果"},
-	{ id:111, pId:11, name:"蔬菜"},
-	{ id:112, pId:11, name:"苹果"},
-	{ id:113, pId:11, name:"大蒜"},
-	{ id:114, pId:11, name:"白菜"},
-	{ id:115, pId:11, name:"青菜"},
-	{ id:12, pId:1, name:"手机数码"},
-	{ id:121, pId:12, name:"手机 "},
-	{ id:122, pId:12, name:"照相机 "},
-	{ id:13, pId:1, name:"电脑配件"},
-	{ id:131, pId:13, name:"手机 "},
-	{ id:122, pId:13, name:"照相机 "},
-	{ id:14, pId:1, name:"服装鞋帽"},
-	{ id:141, pId:14, name:"手机 "},
-	{ id:42, pId:14, name:"照相机 "},
-];
-		
-var code;
-		
-function showCode(str) {
-	if (!code) code = $("#code");
-	code.empty();
-	code.append("<li>"+str+"</li>");
-}
-		
-$(document).ready(function(){
-	var t = $("#treeDemo");
-	t = $.fn.zTree.init(t, setting, zNodes);
-	demoIframe = $("#testIframe");
-	demoIframe.bind("load", loadReady);
-	var zTree = $.fn.zTree.getZTreeObj("tree");
-	zTree.selectNode(zTree.getNodeByParam("id",'11'));
-});	
+		});
+		$('[data-rel="tooltip"]').tooltip({ placement: tooltip_placement });
+		function tooltip_placement(context, source) {
+			var $source = $(source);
+			var $parent = $source.closest('table')
+			var off1 = $parent.offset();
+			var w1 = $parent.width();
+
+			var off2 = $source.offset();
+			var w2 = $source.width();
+
+			if (parseInt(off2.left) < parseInt(off1.left) + parseInt(w1 / 2)) return 'right';
+			return 'left';
+		}
+	})
 </script>
